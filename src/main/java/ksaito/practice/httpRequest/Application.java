7eaab72a1e202ac22f8c293bf8018138efb347d2
Kyohei Saito <saihe.kyon@gmail.com>
2021-04-16 10:23:06 +0900

    Incoming API は読んだけど、エラーになる状態。

--- src/main/java/ksaito/practice/httpRequest/Application.java
+++ src/main/java/ksaito/practice/httpRequest/Application.java
@@ -3,8 +3,11 @@ package ksaito.practice.httpRequest;
 import java.io.IOException;
 
 import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.json.JsonHttpContent;
+import com.google.api.client.testing.json.MockJsonFactory;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
@@ -25,7 +28,13 @@ public class Application {
       .createRequestFactory();
     try {
       val response = httpRequestFactory
-        .buildGetRequest(new GenericUrl("https://google.co.jp"))
+        .buildPostRequest(
+          new GenericUrl("https://leben01.webhook.office.com/webhookb2/59523e37-d403-47e7-b24d-a2bc3ee282b4@bd159a5c-bd99-430a-91da-e170f3d1edaa/IncomingWebhook/57f71409b4ca47eaaea3d754c1d98371/e5b3d776-7d3d-4867-93e1-22ededd6f22f")
+          , new JsonHttpContent(
+            new MockJsonFactory()
+            , "{'text': 'WebHookのテスト'}"
+          )
+        )
         .execute();
       log.info(response.parseAsString());
     } catch (IOException e) {
